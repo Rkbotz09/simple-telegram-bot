deab247cb33a023b69c3a815e6c1cb8fd9a38d3d
Rkbotz09 <128734321+Rkbotz09@users.noreply.github.com>
2023-04-17 12:48:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ def home():
 
 # Start the Flask app
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run()
 
 # Start the Pyrogram client
 client.start()
